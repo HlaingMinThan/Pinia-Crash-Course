e5085dd31248fa74fe2d98612881ff3333c137fb
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-07-30 19:03:25 +0630

    Ep-6-Tailwind-Vue-Vite-Setup

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
-  <h1>Count - {{count}}</h1>
-  <h1>Double Count - {{doubleCount}} </h1>
-  <button @click="increase">increase</button>
+   <h1 class="text-3xl font-bold underline text-blue-600">
+    Hello world!
+  </h1>
 </template>
 
 <script>
-import {mapActions, mapState} from 'pinia';
-import {useCounter} from './store/useCounter';
-
 export default {
-  computed:{
-    myName() {
-      return "Hlaing min than";
-    },
-    ...mapState(useCounter,['count','doubleCount'])
-  },
-  methods : {
-    ...mapActions(useCounter,['increase']),
-  }
+
 }
 </script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import "./style.css";
 import { createPinia } from 'pinia'
 const pinia = createPinia();
 
